ac2794504b8d065ca8493de295ecaf7ecdcc098b
shiv patel <shiv352003@gmail.com>
2024-05-08 12:17:53 +0530

    longName api change

--- api/StrategyManagement/services/InsertdldataEureka.js
+++ api/StrategyManagement/services/InsertdldataEureka.js
@@ -4,19 +4,24 @@ const stat = require("simple-statistics");
 const fs = require("fs");
 // const { PCA } = require("ml-pca");
 const dataset = require("ml-dataset-iris").getNumbers();
-const PCA = require('pca-js')
-var cov = require( 'compute-covariance' );
+const PCA = require("pca-js");
+var cov = require("compute-covariance");
+var MeanVarianceEfficientFrontier = require("portfolio-allocation");
 
 const getData = async (id) => {
   const query = `SELECT stock FROM strategy_Eureka WHERE id = ${id}`;
   const data = await ExecuteQuery(query);
+  console.log(data.length);
 
   const temp_data_array = [];
+  const predicted_change_array = [];
 
   for (const row of data) {
+    console.log(row["stock"]);
     const stock_no = row["stock"];
+    console.log(stock_no);
 
-    const query = `SELECT column_name FROM information_schema.columns WHERE table_name = 'master_benchmarks_price' AND column_name LIKE '%${stock_no}'`;
+    const query = `SELECT column_name FROM information_schema.columns WHERE table_name = 'master_benchmarks_price' AND column_name LIKE '%${encodeURIComponent(stock_no)}'`;
     const result = await ExecuteQuery(query);
 
     const columnName = result[0].column_name;
@@ -28,7 +33,19 @@ const getData = async (id) => {
       value_array.push(parseFloat(value[columnName]));
     }
     temp_data_array.push(value_array);
-    // console.log(data_result.length);
+
+    const q = `SELECT * FROM dl_jobs WHERE strategy_id=${id} AND security=${row["stock"]}`;
+    const data = await ExecuteQuery(q);
+    const sortedData = data.sort(
+      (a, b) => new Date(b.date_completed) - new Date(a.date_completed)
+    );
+    const latestPrediction = sortedData[0].predict_percentage;
+    predicted_change_array.push(parseFloat(latestPrediction));
+
+    // for(const value of data){
+    //   predicted_change_array.push(parseFloat(value.predict_percentage));
+    // }
+    // console.log(data);
   }
 
   let min_size = temp_data_array[0].length;
@@ -51,10 +68,12 @@ const getData = async (id) => {
     }
     percentage_change_array.push(percentage_change);
   }
+  console.log(predicted_change_array);
 
   return {
     final_data: final_data,
     percentage_change_array: percentage_change_array,
+    predicted_change_array: predicted_change_array,
   };
 };
 
@@ -74,18 +93,18 @@ const InsertdldataEureka = async (id) => {
       //   a
       // ];
       // resolve({"predict" : pca.predict(newPoints) , "getExplained" : pca.getExplainedVariance()});
-      
 
       /* ----------------------------------------------------------------------------------------------------- */
+
       // resolve(data.final_data);
       const normalize_data = [];
 
-      for(const array of data.final_data){
+      for (const array of data.final_data) {
         const temp_array = [];
         const mean = array.reduce((acc, val) => acc + val, 0) / array.length;
 
         for (const value of array) {
-            temp_array.push(value - mean);
+          temp_array.push(value - mean);
         }
 
         normalize_data.push(temp_array);
@@ -94,21 +113,21 @@ const InsertdldataEureka = async (id) => {
       // resolve(normalize_data)
 
       const mat = cov(data.percentage_change_array);
+      const result = [mat, data.predicted_change_array];
+      // resolve(result);
 
+      // var vectors = PCA.getEigenVectors(mat);
+      // // resolve(vectors);
+      // for(let i=0;i<vectors.length;i++){
+      //   for(let j=0;j<vectors[i].vector.length;j++){
+      //       console.log(vectors[i].vector[j]);
+      //   }
+      //   console.log("");
+      // }
+      // // resolve(vectors[0].vector[0]);
+      // const x= PCA.computeVarianceCovariance(data);
+      // resolve(x);
 
-      // resolve(cov(data.percentage_change_array));
-
-      var vectors = PCA.getEigenVectors(mat);
-      // resolve(vectors);
-      for(let i=0;i<vectors.length;i++){
-        for(let j=0;j<vectors[i].vector.length;j++){
-            console.log(vectors[i].vector[j]);
-        }
-        console.log("");
-      }
-      // resolve(vectors[0].vector[0]);
-      const x= PCA.computeVarianceCovariance(data);
-      resolve(x);
       // resolve(PCA.computeVarianceCovariance(data));
       // var first = PCA.computePercentageExplained(vectors,vectors[0])
       // var two = PCA.computePercentageExplained(vectors,vectors[0],vectors[1])
@@ -116,6 +135,9 @@ const InsertdldataEureka = async (id) => {
       // var four = PCA.computePercentageExplained(vectors,vectors[0],vectors[1],vectors[2],vectors[3])
       // var five = PCA.computePercentageExplained(vectors,vectors[0],vectors[1],vectors[2],vectors[3],vectors[4])
       // resolve({first,two,three,four,five});
+
+      const x = MeanVarianceEfficientFrontier(mat,data.predicted_change_array);
+      resolve(x);
 
     } catch (error) {
       console.log(error);
--- api/StrategyManagement/services/getLongName.js
+++ api/StrategyManagement/services/getLongName.js
@@ -4,13 +4,15 @@ const yahooFinance = require("yahoo-finance2").default;
 const getLongName = async (stock) => {
   return new Promise(async (resolve, reject) => {
     if (stock == stock.split(".")[0]) {
-      const query = `SELECT column_name FROM information_schema.columns WHERE table_name = 'master_benchmarks_price' AND column_name LIKE '%${stock}'`;
+      const query = `SELECT column_name FROM information_schema.columns WHERE table_name = 'master_benchmarks_price' AND column_name LIKE '%${encodeURIComponent(stock)}'`;
       const result = await ExecuteQuery(query);
 
       const columnName = result[0].column_name;
-      longname = columnName
+      stock_name = columnName
         .substring(0, columnName.lastIndexOf("_"))
         .replace(/_/g, " ");
+      
+      const longname = stock_name ? stock_name:stock;
 
       resolve({
         longname
