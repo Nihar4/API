a7319a9377b77728c11c0b941850a5f34c7e2366
Nihar4 <niharpatel4444@gmail.com>
2024-07-25 17:23:59 +0530

    Change validation of the Stock to add in the strategy

--- api/StrategyManagement/services/validateStock.js
+++ api/StrategyManagement/services/validateStock.js
@@ -8,41 +8,41 @@ const validateStock = async (stock) => {
     // resolve(true);
     // return;
     // }
-    if(stock.split(".")[1]=="EH"){
+    if (stock.split(".")[1] == "EH") {
 
       const query = `SELECT COLUMN_NAME
       FROM INFORMATION_SCHEMA.COLUMNS
       WHERE TABLE_NAME = 'master_benchmarks_price'
       AND COLUMN_NAME LIKE '%${stock.split(".")[0]}'`;
-      
+
       const data = await ExecuteQuery(query);
-      if(data.length>0){
+      if (data.length > 0) {
         resolve(true);
         return;
       }
     }
-    else{
+    else {
 
 
 
-    let calculatedStartDate = new Date(1970, 0, 1);
-    calculatedStartDate = calculatedStartDate.getTime() / 1000;
-    let symbolToPass = stock;
-    const finalEndDate = new Date().getTime() / 1000;
+      let calculatedStartDate = new Date(1970, 0, 1);
+      calculatedStartDate = calculatedStartDate.getTime() / 1000;
+      let symbolToPass = stock;
+      const finalEndDate = new Date().getTime() / 1000;
 
-    const historicalData = await yahooFinance.historical(symbolToPass, {
-      period1: calculatedStartDate,
-      period2: finalEndDate,
-    });
+      const historicalData = await yahooFinance.historical(symbolToPass, {
+        period1: calculatedStartDate,
+        period2: finalEndDate,
+      });
 
-    if (historicalData.length < 1200) {
-      // console.log(historicalData.length);
-      resolve(false);
-    } else {
-      // console.log(historicalData.length);
-      resolve(true);
+      if (historicalData.length < 500) {
+        // console.log(historicalData.length);
+        resolve(false);
+      } else {
+        // console.log(historicalData.length);
+        resolve(true);
+      }
     }
-  }
   });
 };
 
