43419f11e03c07750edc3be47d47e73b70f28d62
Nihar4 <niharpatel4444@gmail.com>
2024-10-01 16:26:23 +0530

    Added Test Uplaod Function

--- api/StrategyManagement/index.js
+++ api/StrategyManagement/index.js
@@ -1,8 +1,61 @@
+const multer = require("multer");
 const { AddStrategyController_Eureka, GetAllStrategiesController_Eureka, DeleteStrategyController_Eureka, GetStrategyController_Eureka, InsertDlDataEureka, getDlDataForHedgeIndex, update_WeightsController, update_percentageController } = require("./controllers/EurekaStrategyManagement");
 const { AddStrategyController, GetAllStrategiesController, DeleteStrategyController, GetStrategyController, getDlData, postDlData, getStockInfo, getChartDataDetails, ValidateStock, jobqueue, getJobQueue, getlongname, update_WeightsController_asset, GetScatterChartDataController, update_PercentageController_asset, GetPerformanceDataController, UpdatePortfolioController, GetPortfolioController, GetPortfolioChartController, getStrategyStockInfoController, DeletePortfolioStrategyController, GetPortfolioStrategyController, GetAllPortfolioStrategiesController, AddPortfolioStrategyController, GetPortfolioTradesController, GetPortfolioCashController, UpdatePortfolioCashController, DeletePortfolioTradesController, InsertPortfolioTradesController, UpdatePortfolioTradesController, ProcessPortfolioTradesController, BulkUpdatePortfolioTradesController, getStrategyStocksController, UpdateStrategyController, AddStrategyStockController, DeleteStrategyStockController } = require("./controllers/StrategyManagement");
 
 const StrategyManagement = require("express").Router()
 
+const fs = require("fs");
+const path = require("path");
+const UploadToAwsBucket = require("../../utils/UploadToAwsBucket");
+const GenerateID = (length) => {
+    return Math.floor(Math.random() * Math.pow(10, length)).toString().padStart(length, '0');
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, "../../Uploads/"));
+    },
+    filename: function (req, file, cb) {
+        console.log("Original File Name", file.originalname);
+        if (!file.fieldname) {
+            cb(null);
+        }
+        const filename = GenerateID(13) + file.originalname.match(/\..*$/)[0];
+        req.body[file.fieldname] = filename;
+        cb(null, filename)
+    },
+});
+
+const multiple_upload = multer({ storage }).any();
+
+
+async function UploadFile(filename) {
+    return new Promise(async (resolve, reject) => {
+        const file_url = await UploadToAwsBucket(filename);
+        console.log("Upload S3 URL ", file_url);
+        resolve(file_url);
+    })
+}
+
+const UploadTestController = async (req, res) => {
+    try {
+        if (!req.files || req.files.length === 0) {
+            return res.status(400).json({ message: "No files uploaded" });
+        }
+
+        const uploadedFile = req.files[0];
+        const fileName = uploadedFile.filename;
+        console.log("Uploaded file name:", fileName);
+
+        const file_url = await UploadFile(fileName);
+        res.status(200).json({ file_url });
+    } catch (error) {
+        console.error("Error uploading file:", error);
+        res.status(500).json({ message: "File upload failed", error });
+    }
+};
+
+StrategyManagement.post("/upload-test", multiple_upload, UploadTestController)
 
 
 StrategyManagement.post("/insert", AddStrategyController)
---
+++ utils/UploadToAwsBucket.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const { S3Client } = require("@aws-sdk/client-s3");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
+
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({
+  path: path.join(__dirname, "../../.env"),
+});
+
+const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+const AWS_BUCKET_FILE_PATH = process.env.AWS_BUCKET_FILE_PATH;
+
+console.log(AWS_ACCESS_KEY)
+const s3_client = new S3Client({
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+  },
+  region: process.env.AWS_REGION,
+});
+
+async function UploadToAwsBucket(fileName) {
+  try {
+    console.log(path.join(__dirname, '..', 'Uploads', fileName))
+    const fileContent = fs.readFileSync(path.join(__dirname, '..', 'Uploads', fileName));
+
+    const mimeTypes = {
+      '.pdf': 'application/pdf',
+      '.jpg': 'image/jpeg',
+      '.jpeg': 'image/jpeg',
+      '.png': 'image/png',
+    };
+
+    const fileExtension = fileName.substring(fileName.lastIndexOf('.'));
+    const contentType = mimeTypes[fileExtension.toLowerCase()] || 'application/octet-stream';
+    const command = new PutObjectCommand({
+      Bucket: AWS_BUCKET_NAME,
+      Key: `${AWS_BUCKET_FILE_PATH}/${fileName}`,
+      Body: fileContent,
+      ACL: "public-read",
+      ContentType: contentType
+    });
+
+    await s3_client.send(command);
+    fs.unlink((path.join(__dirname, '..', 'Uploads', fileName)), (err) => { console.log(err) });
+
+    return `https://${AWS_BUCKET_NAME}.s3.amazonaws.com/${AWS_BUCKET_FILE_PATH}/${fileName}`;
+
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+module.exports = UploadToAwsBucket;
